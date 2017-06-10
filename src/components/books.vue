87338951eab141ab7e7341391d6ad05246c05103
harry bournis <harrybournis@gmail.com>
2017-06-10 15:02:50 +0300

    styled books page

--- src/components/books.vue
+++ src/components/books.vue
@@ -1,10 +1,64 @@
 <template lang="pug">
-h1.center-align Books
+.container
+  .row
+    .col.s12
+      h2.center-align Books
+    .col.s12
+      ul.collection
+        li.collection-item.avatar("v-for"="book in books")
+          i.material-icons.circle(":class"="redOrGreen(book)") {{ book.isInstock ? 'assignment_turned_in' : 'assignment_late' }}
+          span.title.booktitle {{ book.title }}
+          p
+            | by: {{ book.author }}
+            br
+            | ISBN: {{ book.isbn }}
+            br
+            | Subject: {{ book.subject }}
+            br
+            | Publisher: {{ book.publisher }}
+          a.secondary-content(href='#!')
+            i.material-icons create
 </template>
 
 <script>
 	export default {
-		name: "Books"
+		name: "Books",
+		data () {
+			return {
+				books: [
+					{
+						id: 1,
+						title: "title1",
+						author: "author1",
+						language: "language",
+						isbn: "23213425",
+						subject: "Horror",
+						isInstock: true,
+						publisher: "publisher1"
+					},
+					{
+						id: 2,
+						title: "title2",
+						author: "author2",
+						language: "language",
+						isbn: "23213425",
+						subject: "Horror",
+						isInstock: false,
+						publisher: "publisher2"
+					}
+				],
+				redOrGreen (book) {
+					return {
+						red: !book.isInstock,
+						green: book.isInstock
+					}
+				}
+			}
+		}
 	}
 </script>
-<style scoped></style>
+<style scoped>
+	.collection .collection-item.avatar .title.booktitle {
+		font-size: 1.4rem;
+	}
+</style>
